420a0437903176cee044a2173a0f3d28828ac935
Reemaalhawas <93104215+Reemaalhawas@users.noreply.github.com>
2025-02-24 05:22:04 +0300

    add enqueue method

--- FCFSscheduler.js
+++ FCFSscheduler.js
@@ -21,3 +21,21 @@ class Queue {
     this.length = 0;
   }
 }
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  enqueue(value) {
+    const newNode = new ListNode(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+}
